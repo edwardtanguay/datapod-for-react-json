d36c5a9c8665faaecbaa73f48a6218cc4090527b
Edward Tanguay (windows) <edward@tanguay.info>
2025-03-31 10:40:00 +0200

    cleaned

--- src/store/dataModel.ts
+++ src/store/dataModel.ts
@@ -1,5 +1,5 @@
 import { FlashcardSchema, FrontendFlashcard } from "../types";
-import rawFlashcards from "../../datajson/flashcards.json";
+import rawFlashcards from "../../data/flashcards.json";
 
 export const getFlashcards = () => {
 	const frontendFlashcards: FrontendFlashcard[] = [];
